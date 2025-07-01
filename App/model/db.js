9d70f3b69893e4d0926467a285e6df0c67b1f3a1
jaaditya <jaiswaladit577@gmail.com>
2025-07-01 16:04:00 +0545

    done

--- App/model/db.js
+++ App/model/db.js
@@ -1,19 +1,19 @@
 require("dotenv").config();
 const { Client } = require("pg");
 
-// const conn = new Client({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   port: process.env.DB_PORT,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-// });
 const conn = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false, // Required for Render PostgreSQL
-  },
+  host: process.env.HOST,
+  user: process.env.USER,
+  port: process.env.PORT,
+  password: process.env.PASSWORD,
+  database: process.env.NAME,
 });
+// const conn = new Client({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false, // Required for Render PostgreSQL
+//   },
+// });
 (async () => {
   try {
     await conn.connect();
